642a4d062695d4e55e27c22d5c35c3340ccb8f35
Agrawalarchi <114734528+Agrawalarchi@users.noreply.github.com>
2025-08-06 16:33:19 +0530

    Update Lottery.jsx

--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -3,7 +3,7 @@ import "./Lottery.css"
 import { useState } from "react";
 import { genTicket, sum } from "./helper";
 import Button from "./Button";
-import { conditions } from "./Condition.jsx";
+import { conditions } from "./Condition.js";
 
 export default function Lottery({ n = 4 }) {
   const [ticketArr, setTicketArr] = useState(genTicket(n));
